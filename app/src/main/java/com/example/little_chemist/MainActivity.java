687b27dacdbde8cfcf70432402c5231e671cc9a7
Esraa <esraa125@hotmail.com>
2020-01-29 11:53:01 +0300

    ii

--- app/src/main/java/com/example/little_chemist/MainActivity.java
+++ app/src/main/java/com/example/little_chemist/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
