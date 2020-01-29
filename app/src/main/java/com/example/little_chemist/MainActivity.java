d80620ad71c14305bb52e5ebadad7dea9c4d402f
noura <noura-619@hotmail.com>
2020-01-29 15:38:29 +0300

    Working on Start Screen animation

--- app/src/main/java/com/example/little_chemist/MainActivity.java
+++ app/src/main/java/com/example/little_chemist/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.little_chemist;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.little_chemist.ui.login.Login;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim, bottomAnim;
+    androidx.appcompat.widget.AppCompatImageView image;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +32,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        image = findViewById(R.id.imageview);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom);
+        image.setAnimation(topAnim);
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent intent = new Intent(MainActivity.this, Login.class);
+                                          startActivity(intent);
+                                          finish();
 
+                                      }
+                                  }, //Pass time here
+                SPLASH_SCREEN);
         /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.+OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
