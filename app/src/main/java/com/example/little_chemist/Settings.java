53dd25bb8cbd031ed1b06e9c7bf042b570bc7da9
noura <noura-619@hotmail.com>
2020-01-30 14:12:18 +0300

    connected settings to home Updated language

--- app/src/main/java/com/example/little_chemist/Settings.java
+++ app/src/main/java/com/example/little_chemist/Settings.java
@@ -7,7 +7,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.little_chemist.ui.login.Login;
+import java.util.Locale;
+
+import android.os.Bundle;
+
+import android.app.Activity;
+
+import android.content.Intent;
+
+import android.content.res.Configuration;
+
+import android.content.res.Resources;
+
+import android.util.DisplayMetrics;
+
+import android.view.View;
+
+import android.widget.AdapterView;
+
+import android.widget.Button;
+
+
 
 public class Settings extends AppCompatActivity {
 
@@ -16,6 +36,9 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        Button En = findViewById(R.id.button3);
+        Button Ara = findViewById(R.id.button4);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -27,5 +50,22 @@ public class Settings extends AppCompatActivity {
                 finish();
             }
         });
+        En.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+        Ara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
     }
+
 }
