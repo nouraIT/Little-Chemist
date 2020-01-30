19027618d8d5423acdc8faef59485f115c03386b
noura <noura-619@hotmail.com>
2020-01-30 12:34:45 +0300

    started Log in and sign up

--- app/src/main/java/com/example/little_chemist/Home.java
+++ app/src/main/java/com/example/little_chemist/Home.java
@@ -3,12 +3,16 @@ package com.example.little_chemist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
-
+private ImageView set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        set = findViewById(R.id.settings);
+
     }
+
 }
---
+++ app/src/main/java/com/example/little_chemist/Settings.java
@@ -0,0 +1,31 @@
+package com.example.little_chemist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.little_chemist.ui.login.Login;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Homepage = new Intent(Settings.this, Home.class);
+                startActivity(Homepage);
+                finish();
+            }
+        });
+    }
+}
