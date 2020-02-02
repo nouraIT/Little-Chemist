08c2e8ae4efda2a2921f792910f33cfb19e32ed1
sarahsaad2329 <56547239+sarahsaad2329@users.noreply.github.com>
2020-02-02 18:18:06 +0300

    chapters interfaces with main chpter interface

---
+++ app/src/main/java/com/example/little_chemist/Chapters_dir/Ch2.java
@@ -0,0 +1,16 @@
+package com.example.little_chemist.Chapters_dir;
+
+import android.os.Bundle;
+
+import com.example.little_chemist.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Ch2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch2);
+    }
+}
