8168da82e6c31a0b60e04c61543e59ab471e72f3
sarahsaad2329 <56547239+sarahsaad2329@users.noreply.github.com>
2020-02-02 21:03:01 +0300

    connect chapters with home layout

--- app/src/main/java/com/example/little_chemist/Home.java
+++ app/src/main/java/com/example/little_chemist/Home.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 
 public class Home extends AppCompatActivity {
 private ImageView set;
+    private Button button ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,16 @@ private ImageView set;
 
             public void onClick(View view) {
                 Intent n = new Intent(Home.this, Settings.class);
+                startActivity(n);
+                finish();
+            }
+        });
+
+
+        button = findViewById(R.id.button3) ;
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent n = new Intent(Home.this, Chapters.class);
                 startActivity(n);
                 finish();
             }
