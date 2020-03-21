1ff3c8f3663b8f3ccc543713ff8248effaa8c68e
noura <noura-619@hotmail.com>
2020-03-21 17:15:26 +0300

    lab lessons

--- app/src/main/java/com/example/little_chemist/LabLesson4.java
+++ app/src/main/java/com/example/little_chemist/LabLesson4.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import android.app.AlertDialog;
