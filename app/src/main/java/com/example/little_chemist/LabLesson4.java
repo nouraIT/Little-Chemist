22c9a809f8a446e897db8401915a6836b4bc779d
noura <noura-619@hotmail.com>
2020-03-21 16:02:24 +0300

    lab lessons

--- app/src/main/java/com/example/little_chemist/LabLesson4.java
+++ app/src/main/java/com/example/little_chemist/LabLesson4.java
@@ -4,14 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import android.app.AlertDialog;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.*;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.solver.widgets.Rectangle;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Timer;
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.SkeletonNode;
 import com.google.ar.sceneform.animation.ModelAnimator;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 public class LabLesson4 extends AppCompatActivity {
     int count=0;
@@ -28,37 +67,249 @@ public class LabLesson4 extends AppCompatActivity {
     private ModelRenderable mObjRenderable;
     private int i=0;
 
+    //for net movement along x-axis
+    public float x = 0;
+    public float y = 0;
+    private ModelLoader modelLoader;
+    private RelativeLayout myLayout = null;
+    //Screen Size
+    private int screenWidth;
+    private int screenHeight;
+    //Position
+    private float ballDownY;
+    private float ballDownX;
+    //Initialize Class
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
+    //Images
+    private ImageView net = null;
+    private ImageView ball = null;
+    //score
+    private TextView score = null;
+    //points
+    private int points = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab_lesson4);
         arFragment = (ARfragment5) getSupportFragmentManager().findFragmentById(R.id.arFragment);
         textView = findViewById(R.id.textview);
-        textView.setText(R.string.tap_call4);
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            createModel(hitResult.createAnchor(),arFragment);
+        Button Reset = findViewById(R.id.reset);
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Reset Code
+                recreate();
+            }
         });
+        arFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+            arFragment.onUpdate(frameTime);
+            onUpdate();
+        });
+
+        modelLoader = new ModelLoader(new WeakReference<>(this));
+
+        initializeGallery();
+
+
     }
-    private void createModel(Anchor anchor, ARfragment5 arFragment) {
 
-        ModelRenderable.builder()
-                .setSource(this, Uri.parse("gas.sfb"))
-                .build()
-                .thenAccept(modelRenderable -> {
-                    AnchorNode anchorNode = new AnchorNode(anchor);
-                    SkeletonNode skeletonNode = new SkeletonNode();
-                    skeletonNode.setParent(anchorNode);
-                    skeletonNode.setRenderable(modelRenderable);
-                    arFragment.getArSceneView().getScene().addChild(anchorNode);
-                    //textView.setText(R.string.co2);
+    private void onUpdate() {
+        boolean trackingChanged = updateTracking();
+        View contentView = findViewById(android.R.id.content);
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.getOverlay().add(pointer);
+            } else {
+                contentView.getOverlay().remove(pointer);
+            }
+            contentView.invalidate();
+        }
 
+        if (isTracking) {
+            boolean hitTestChanged = updateHitTest();
+            if (hitTestChanged) {
+                pointer.setEnabled(isHitting);
+                contentView.invalidate();
+            }
+        }
+    }
 
-                })
-                .exceptionally(
-                        throwable -> {
+    private boolean updateTracking() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        boolean wasTracking = isTracking;
+        isTracking = frame != null &&
+                frame.getCamera().getTrackingState() == TrackingState.TRACKING;
+        return isTracking != wasTracking;
+    }
 
+    private boolean updateHitTest() {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        boolean wasHitting = isHitting;
+        isHitting = false;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    isHitting = true;
+                    break;
+                }
+            }
+        }
+        return wasHitting != isHitting;
+    }
+
+    private android.graphics.Point getScreenCenter() {
+        View vw = findViewById(android.R.id.content);
+        return new android.graphics.Point(vw.getWidth()/2, vw.getHeight()/2);
+    }
+
+    private void initializeGallery() {
+        RelativeLayout gallery = findViewById(R.id.gallery);
+
+        ImageView andy = new ImageView(this);
+       // andy.setImageResource(R.drawable.droid_thumb);
+        andy.setTooltipText(getString(R.string.gas));
+        andy.setContentDescription(getString(R.string.selectgas));
+        andy.setOnClickListener(view ->{addObject(Uri.parse("gas.sfb"));});
+        gallery.addView(andy);
+
+        ImageView cabin = new ImageView(this);
+        andy.setTooltipText(getString(R.string.liquid));
+        cabin.setContentDescription(getString(R.string.selectliquid));
+    cabin.setOnClickListener(view ->{addObject(Uri.parse("liquid.sfb"));});
+        gallery.addView(cabin);
+//
+//        ImageView house = new ImageView(this);
+//        house.setImageResource(R.drawable.house_thumb);
+//        house.setContentDescription("house");
+//        house.setOnClickListener(view ->{addObject(Uri.parse("House.sfb"));});
+//        gallery.addView(house);
+
+    }
+
+    private void addObject(Uri model) {
+        Frame frame = arFragment.getArSceneView().getArFrame();
+        android.graphics.Point pt = getScreenCenter();
+        List<HitResult> hits;
+        if (frame != null) {
+            hits = frame.hitTest(pt.x, pt.y);
+            for (HitResult hit : hits) {
+                Trackable trackable = hit.getTrackable();
+                if (trackable instanceof Plane &&
+                        ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+                    modelLoader.loadModel(hit.createAnchor(), model);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    public void addNodeToScene(Anchor anchor, ModelRenderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+// Maxscale must be greater than minscale
+
+//        node.getScaleController().setMaxScale(0.09f);
+        node.getScaleController().setMinScale(0.05f);
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+    public void onException(Throwable throwable){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(throwable.getMessage())
+                .setTitle("Codelab error!");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return;
+    }
+
+    public void Render()
+    {
+        changePos();
+        if(Collision(net, ball))
+        {
+            points++; //You dont need findView Textview score for that exists in OnCreate Method
+            this.score.setText("Score:" + points);
+        }
+    }
+
+    public void changePos() {
+
+        //down
+        ballDownY += 10;
+        if (ball.getY() > screenHeight) {
+            ballDownX = (float) Math.floor((Math.random() * (screenWidth - ball.getWidth())));
+            ballDownY = -100.0f;
+
+        }
+        ball.setY(ballDownY);
+        ball.setX(ballDownX);
+
+        //make net follow finger
+        myLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                x = event.getX();
+                y = event.getY();
+
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    net.setX(x);
+                    net.setY(y);
+                }
+                return true;
+            }
+
+        });
+
+    }
+
+    public boolean Collision(ImageView net, ImageView ball){
+        Rect BallRect = new Rect();
+        ball.getHitRect(BallRect);
+        Rect NetRect = new Rect();
+        net.getHitRect(NetRect);
+        return BallRect.intersect(NetRect);
+    }
+
+    public class ModelLoader {
+        private static final String TAG = "ModelLoader";
+        private final WeakReference<LabLesson4> owner;
+
+        ModelLoader(WeakReference<LabLesson4> owner) {
+            this.owner = owner;
+        }
+
+        void loadModel(Anchor anchor, Uri uri) {
+            if (owner.get() == null) {
+                Log.d(TAG, "Activity is null.  Cannot load model.");
+                return;
+            }
+            ModelRenderable.builder()
+                    .setSource(owner.get(), uri)
+                    .build()
+                    .handle((renderable, throwable) -> {
+                        LabLesson4 activity = owner.get();
+                        if (activity == null) {
                             return null;
-                        });
-    }
+                        } else if (throwable != null) {
+                            activity.onException(throwable);
+                        } else {
+                            activity.addNodeToScene(anchor, renderable);
+                        }
+                        return null;
+                    });
 
+            return;
+        }
+    }
 }
